a5af7e55209ea9135bf7b1dd75ca94914f59911d
shivansh-bhatnagar18 <shivansh111sid@gmail.com>
2024-11-14 23:49:17 +0530

    Basic Structure of the page1 is imlemented

--- dream11modelui/app/page.tsx
+++ dream11modelui/app/page.tsx
@@ -7,15 +7,31 @@ import { Button } from '@mui/material';
 export default function Home() {
   const lightTheme = createTheme({
     palette: {
-      mode: 'light',
+      mode: 'light',  // Set light mode explicitly
+      primary: {
+        main: '#1976d2',  // Customize the primary color
+      },
+      secondary: {
+        main: '#ff4081',  // Customize the secondary color
+      },
       background: {
-        default: '#ffffff',
-        paper: '#ffffff',
+        default: '#ffffff',  // Set background for light mode
+        paper: '#f5f5f5',
       },
       text: {
         primary: '#000000',
-      },
-    },
+        secondary: '#4f4f4f',
+      },},
+      components: {
+        MuiButton: {
+          styleOverrides: {
+            root: {
+              color: '#fff',   // Change text color for Button component in light mode
+              backgroundColor: '#7FA601',  // Background color for button
+            },
+          },
+        },
+      }
   });
   
   const periodData = [
@@ -29,6 +45,13 @@ export default function Home() {
     {label : "08/01/24 - 08/31/24"},
     {label : "09/01/24 - 09/30/24"},
   ]
+  
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    console.log('Form submitted');
+    // Handle form submission logic here
+  };
+
   return (
     <ThemeProvider theme={lightTheme}>
       <CssBaseline />
@@ -37,20 +60,21 @@ export default function Home() {
         <div className="mt-10 font-bold font-barlow text-5xl text-center w-screen">
           DREAM11
         </div>
-        <div className = "flex flex-col items-center">
+        <div className="flex flex-col items-center">
           <div className="mt-20 font-barlow font-bold text-2xl">
             SELECT YOUR TRAINING AND TESTING PERIOD
           </div> 
+          <form onSubmit={handleSubmit} className="flex flex-col items-center">
             <div className='pt-10'>
-              <SearchDropdown label="Training Period" periodData={periodData} style={{ color: '#000000' }} />
+              <SearchDropdown label="Training Period" periodData={periodData} />
             </div>
-            <div className= 'pt-1'>
+            <div className='pt-1'>
               <SearchDropdown label="Testing Period" periodData={periodData} />
             </div>
-            <Button variant="contained" color="primary" className="mt-10">
+            <Button type="submit" variant="contained" color="primary" className="mt-10">
               Submit
             </Button>
-          {/* <SearchDropdown label="Testing Period" periodData={periodData} /> */}
+          </form>
         </div>
       </div>
     </ThemeProvider>
