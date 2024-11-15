f5afbfa4ffc23488a6ed0567f8bf304bb0dcd515
shivansh-bhatnagar18 <shivansh111sid@gmail.com>
2024-11-15 10:01:08 +0530

    Done with all the fixes that were left for yesterday

--- dream11modelui/pages/comparison/index.tsx
+++ dream11modelui/pages/comparison/index.tsx
@@ -3,64 +3,63 @@ import React from 'react'
 import SearchDropdown from '@/components/searchDropdown'
 import PlayerComponent from '@/components/playerAvatar'
 
-
-function page2() {
+function Comparison() {
     const periodData = [
-        {label : "10/01/24 - 11/01/24"},
-        {label : "10/01/24 - 11/01/24"},
-        {label : "10/01/24 - 11/01/24"},
-        {label : "10/01/24 - 11/01/24"},
-        {label : "10/01/24 - 11/01/24"},
-        {label : "10/01/24 - 11/01/24"},
-        {label : "10/01/24 - 11/01/24"},
-        {label : "10/01/24 - 11/01/24"},
-        {label : "10/01/24 - 11/01/24"},
-      ]
+      {label : "01/01/24 - 02/01/24"},
+      {label : "02/01/24 - 03/01/24"},
+      {label : "03/01/24 - 04/01/24"},
+      {label : "04/01/24 - 05/01/24"},
+      {label : "05/01/24 - 06/01/24"},
+      {label : "06/01/24 - 07/01/24"},
+      {label : "07/01/24 - 08/01/24"},
+      {label : "08/01/24 - 09/01/24"},
+      {label : "09/01/24 - 10/01/24"},
+    ]
   return (
-    <div className='flex justify-between'>
-      <div className='left flex flex-col'>
-        <div className='logo1'>
-          logo1
-        </div>
-        <div className='team1 bg-sadium bg-contain bg-no-repeat '>
-          <img src='./stadium.png'/>
-        </div>
-      </div>
-      
-      <div className=' center flex flex-col flex-wrap'>
-        <div className="mt-10 pb-10 font-bold font-barlo text-5xl text-center">
+    <div className='flex justify-between bg-[#000426] h-full xl:h-screen flex-col'>   
+      <div className='flex flex-col flex-wrap lg:z-50 z-10 items-center'>
+        <div className="mt-10 pb-10 font-bold text-8xl text-center">
           DREAM11
         </div>
-        <div className='w-full flex justify-center'>
-          <SearchDropdown label="1" periodData={periodData} />
+        <div className='w-min flex justify-center bg-[#D9D9D9] rounded-l'>
+          <SearchDropdown label="Select Match" periodData={periodData} />
         </div>
-        <div className='data flex flex-col flex-wrap justify-center font-barlo p-10'>
-          <div className='title block text-center text-2xl text-[#757575] font-bold'>FANTASY SCORE</div>
+        <div className='data flex flex-col flex-wrap justify-center p-10'>
+          <div className='title block text-center text-5xl text-[#757575] font-bold'>FANTASY SCORE</div>
           <div className='flex flex-wrap justify-center'>
           <div className='left text-right'>
-            <div className='score font-bold text-7xl text-[#A9A9A9] mr-5'>96</div>
-            <div className='AI text-2xl text-[#757575] font-bold mr-5'>AI</div>
+            <div className='score font-bold text-9xl text-[#A9A9A9] mr-2'>96</div>
+            <div className='AI text-5xl text-[#757575] font-bold mr-2'>AI</div>
           </div>
           <div className='mid h-full w-1 bg-white'></div>
           <div className='right text-left'>
-            <div className='score font-bold text-7xl text-[#A9A9A9] ml-5'>94</div>
-            <div className='actual text-2xl text-[#757575] font-bold ml-5'>REAL</div>
+            <div className='score font-bold text-9xl text-[#A9A9A9] ml-2'>94</div>
+            <div className='actual text-5xl text-[#757575] font-bold ml-2'>REAL</div>
           </div>
           </div>
         </div>
       </div>
-
-      <div className='left flex flex-col'>
-        <div className='logo2'>
-          logo2
+      <div className='flex xl:flex-row z-10 flex-col xl:fixed w-full xl:bottom-20 xl:gap-10 gap-20'>
+        <div className='flex flex-col items-center'>
+          {/* <div className='logo1'>
+            logo1
+          </div> */}
+          <div className='team1 bg-sadium bg-contain bg-no-repeat w-[70%]'>
+            <img src='./stadium.png'/>
+          </div>
         </div>
-        <div className='team1 bg-sadium bg-contain bg-no-repeat '>
-          <img src='./stadium.png' />
+        <div className='flex flex-col items-center'>
+          {/* <div className='logo2'>
+            logo2
+          </div> */}
+          <div className='team1 bg-sadium bg-contain bg-no-repeat w-[70%]'>
+            <img src='./stadium.png' />
+          </div>
+          {/* <PlayerComponent imageSrc='https://cdn.sportmonks.com/images/cricket/players/2/2.png' number={1} name='abs'/> */}
         </div>
-        {/* <PlayerComponent imageSrc='https://cdn.sportmonks.com/images/cricket/players/2/2.png' number={1} name='abs'/> */}
       </div>
     </div>
   )
 }
 
-export default page2
+export default Comparison
---
+++ dream11modelui/pages/mainPage/index.tsx
@@ -0,0 +1,85 @@
+'use client'
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import SearchDropdown from "../../components/searchDropdown";
+import { Button } from '@mui/material';
+
+export default function Mainpage() {
+  const lightTheme = createTheme({
+    palette: {
+      mode: 'light',  // Set light mode explicitly
+      primary: {
+        main: '#1976d2',  // Customize the primary color
+      },
+      secondary: {
+        main: '#ff4081',  // Customize the secondary color
+      },
+      background: {
+        default: '#ffffff',  // Set background for light mode
+        paper: '#f5f5f5',
+      },
+      text: {
+        primary: '#000000',
+        secondary: '#4f4f4f',
+      },},
+      components: {
+        MuiButton: {
+          styleOverrides: {
+            root: {
+              color: '#fff',   // Change text color for Button component in light mode
+              backgroundColor: '#7FA601',  // Background color for button
+            },
+          },
+        },
+      }
+  });
+  
+  const periodData = [
+    {label : "01/01/24 - 01/31/24"},
+    {label : "02/01/24 - 02/28/24"},
+    {label : "03/01/24 - 03/31/24"},
+    {label : "04/01/24 - 04/30/24"},
+    {label : "05/01/24 - 05/31/24"},
+    {label : "06/01/24 - 06/30/24"},
+    {label : "07/01/24 - 07/31/24"},
+    {label : "08/01/24 - 08/31/24"},
+    {label : "09/01/24 - 09/30/24"},
+  ]
+  
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    console.log('Form submitted');
+    // Handle form submission logic here
+  };
+
+  return (
+    <ThemeProvider theme={lightTheme}>
+      <CssBaseline />
+      <div className="h-screen flex justify-center bg-[#000426] flex-wrap opacity-90">
+        {/* <img src='/bg.png' alt="logo" className="w-24 h-24 mt-10" /> */}
+        <div className="mt-10 font-bold text-8xl text-center w-screen">
+          DREAM11
+        </div>
+        <div className="flex flex-col items-center">
+          <div className="font-bold text-5xl">
+            SELECT YOUR TRAINING AND
+          </div>
+          <div className="font-bold text-5xl justify-center">
+            TESTING PERIOD
+          </div> 
+          <form onSubmit={handleSubmit} className="flex flex-col items-center">
+            <div className='mt-11 bg-[#D9D9D9] rounded-l'>
+              <SearchDropdown label="Training Period" periodData={periodData} />
+            </div>
+            <div className='mt-1 bg-[#D9D9D9] rounded-l'>
+              <SearchDropdown label="Testing Period" periodData={periodData} />
+            </div>
+            <Button type="submit" variant="contained" color="primary" className="mt-10">
+              Submit
+            </Button>
+          </form>
+        </div>
+      </div>
+    </ThemeProvider>
+  );
+}
